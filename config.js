3be828b2bbdf22b3dc55459c307e7273ae9b7d15
Juvejona1978 <122318402+Juvejona1978@users.noreply.github.com>
2026-07-13 21:43:01 -0600

    Configurar WhatsApp de Kim

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ const CONFIG = {
   nombreTienda: "Belleza Farmasi",
   eslogan: "Catálogo · Pedidos en línea",
   linkFarmasi: "https://farpaper.farmasi.com/v/e6d9dd06-1462-42b1-ba5e-d87b364931bc",
+  whatsappNumero: "50672914585",
   moneda: "USD"
 };
