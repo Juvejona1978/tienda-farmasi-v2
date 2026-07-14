1463ad19b45e50b39ff0add0862061b53c86b6c2
Juvejona1978 <122318402+Juvejona1978@users.noreply.github.com>
2026-07-13 22:01:16 -0600

    Corregir telefono de WhatsApp

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ const CONFIG = {
   nombreTienda: "Belleza Farmasi",
   eslogan: "Catálogo · Pedidos en línea",
   linkFarmasi: "https://farpaper.farmasi.com/v/e6d9dd06-1462-42b1-ba5e-d87b364931bc",
-  whatsappNumero: "50672914585",
+  whatsappNumero: "50670099715",
   moneda: "USD"
 };
